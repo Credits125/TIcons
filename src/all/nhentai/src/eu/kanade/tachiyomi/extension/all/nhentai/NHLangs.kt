0662fcb9bb6c15d27bd4c755416f1ec4019fde6e
Ankit SIngh <as280093@gmail.com>
2019-02-28 22:55:43 +0530

    Add files via upload

---
+++ src/all/nhentai/src/eu/kanade/tachiyomi/extension/all/nhentai/NHLangs.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.all.nhentai
+
+class NHEnglish : NHentai("en", "english")
+class NHJapanese : NHentai("ja", "japanese")
+class NHChinese : NHentai("zh", "chinese")
